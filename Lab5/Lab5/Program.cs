bf3f4a206c344b0152a715932ef741dd5251ad77
Brandon Simak <S01204602@acad.tri-c.edu>
2019-04-22 19:11:56 -0400

    lab5

---
+++ Lab5/Lab5/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                public double getArea(double hight, double width)
+                {
+                    return hight * width;
+                }
+
+                the scope defines how visible the code is to the rest of the program
+                the scope in this example is "public"
+
+                static means a class can be called with all of its variables already defined
+                a non static class will need variables to be restated if called
+
+                the return type determines what the data in the code is
+                in this example, the return type is a double
+
+                the identifier is a unique name given to a method so that it can be called later in the code
+                the identifier of this code is "getArea"
+
+                the parameters tell the method what kind of data and in what order to expect
+                the parameters here are "double hight" and "double width"
+
+                the method body is the instructions on what to do with the data it gets
+                the body of this example tells it to multiply the hight and width
+
+                the difference between user-defined methods and framework methods are that one is created by a user for whatever it is they need, while framework methods are built in and cannot be changed
+                framework methods should be considered so you do not accidentally create a method under the same identifier
+             */
+
+
+        }
+        public bark()
+        {
+            Console.WriteLine("{0} is barking...", name);
+        }
+
+        public DoTrick(string trickname)
+        {
+            Console.WriteLine("{0} is so smart!  {0} is doing a(n) {1}", name, trickName);
+        }
+    }
+}
